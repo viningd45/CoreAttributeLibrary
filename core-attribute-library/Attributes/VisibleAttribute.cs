f97f40afa8d0de12c6f127f2f97fb54bc176870c
viningd45 <daniel.vining@benteler.com>
2021-03-22 16:02:26 -0500

    Create VisibleAttribute.cs

---
+++ core-attribute-library/Attributes/VisibleAttribute.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Core.Attribute.Library.Attributes
+{
+    [System.AttributeUsage(System.AttributeTargets.Property)]
+    public class VisibleAttribute : System.Attribute, IAttribute
+    {
+        private readonly bool visible;
+
+        public VisibleAttribute(bool visible)
+        {
+            this.visible = visible;
+        }
+
+        public object GetAttributeValue()
+        {
+            return this.visible;
+        }
+    }
+}
